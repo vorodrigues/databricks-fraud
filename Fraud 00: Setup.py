7b23215cb2e2c275d97842dfd70d62ff7af056dc
vct_or@hotmail.com <vct_or@hotmail.com>
2022-04-01 13:06:24 +0000

    test-de

---
+++ Fraud 00: Setup.py
@@ -0,0 +1,91 @@
+# Databricks notebook source
+# MAGIC %md # Dev Tables
+
+# COMMAND ----------
+
+# MAGIC %md ## Database
+
+# COMMAND ----------
+
+# MAGIC %sql CREATE DATABASE IF NOT EXISTS vr_fraud_dev;
+
+# COMMAND ----------
+
+# MAGIC %md ## atm_visits.json
+
+# COMMAND ----------
+
+spark.read.parquet('/mnt/databricks-datasets-private/ML/fighting_atm_fraud/atm_visits') \
+     .write.json('/FileStore/vr/fraud/dev/raw/atm_visits')
+
+# COMMAND ----------
+
+# MAGIC %md ## customers_silver
+
+# COMMAND ----------
+
+# MAGIC %sql CREATE OR REPLACE TABLE vr_fraud_dev.customers_silver AS SELECT * FROM parquet.`/mnt/databricks-datasets-private/ML/fighting_atm_fraud/atm_customers`
+
+# COMMAND ----------
+
+# MAGIC %md ## locations_silver
+
+# COMMAND ----------
+
+# MAGIC %sql CREATE OR REPLACE TABLE vr_fraud_dev.locations_silver AS SELECT * FROM parquet.`/mnt/databricks-datasets-private/ML/fighting_atm_fraud/atm_locations`
+
+# COMMAND ----------
+
+# MAGIC %md # Test Tables
+
+# COMMAND ----------
+
+# MAGIC %md ## Database
+
+# COMMAND ----------
+
+# MAGIC %sql CREATE DATABASE IF NOT EXISTS vr_fraud_test;
+
+# COMMAND ----------
+
+# MAGIC %md ## atm_visits.json
+
+# COMMAND ----------
+
+spark.read.json('/FileStore/vr/fraud/raw/atm_visits') \
+     .limit(10) \
+     .write.json('/FileStore/vr/fraud/test/raw/atm_visits')
+
+# COMMAND ----------
+
+# MAGIC %md ## customers_silver
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC CREATE OR REPLACE TABLE vr_fraud_test.customers_silver AS SELECT
+# MAGIC   a.* 
+# MAGIC FROM vr_fraud_dev.customers_silver AS a
+# MAGIC RIGHT JOIN json.`/FileStore/vr/fraud/test/raw/atm_visits` AS b
+# MAGIC ON a.customer_id = b.customer_id
+
+# COMMAND ----------
+
+# MAGIC %md ## locations_silver
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC CREATE OR REPLACE TABLE vr_fraud_test.locations_silver AS SELECT
+# MAGIC   a.* 
+# MAGIC FROM vr_fraud_dev.locations_silver AS a
+# MAGIC RIGHT JOIN json.`/FileStore/vr/fraud/test/raw/atm_visits` AS b
+# MAGIC ON a.atm_id = b.atm_id
+
+# COMMAND ----------
+
+# MAGIC %md # Prod Tables
+
+# COMMAND ----------
+
+
--- Fraud 03: Model Training.py
+++ Fraud 03: Model Training.py
@@ -19,6 +19,14 @@ db = dbutils.widgets.get('db')
 
 # COMMAND ----------
 
+# MAGIC %sql create table train as select
+# MAGIC   visit_id,
+# MAGIC   case fraud_report when 'Y' then 1 when 'N'then 0 else null end as fraud_report
+# MAGIC from visits_gold
+# MAGIC limit 10000
+
+# COMMAND ----------
+
 from databricks import feature_store
 from databricks.feature_store import FeatureLookup
 
@@ -222,8 +230,8 @@ with mlflow.start_run(run_name='XGBClassifer'):
     fn=evaluate_model,
     space=search_space,
     algo=tpe.suggest,  # algorithm controlling how hyperopt navigates the search space
-    max_evals=1,                             ### INCREASE ###
-    trials=SparkTrials(parallelism=1),       ### INCREASE ###
+    max_evals=3,                             ### INCREASE ###
+    trials=SparkTrials(parallelism=3),       ### INCREASE ###
     verbose=True
   )
 
@@ -299,14 +307,13 @@ model_name = 'VR Fraud Analytics'
 
 # COMMAND ----------
 
-for i in range(3):
-  fs.log_model(
-    wrappedModel,
-    "model",
-    flavor=mlflow.pyfunc,
-    training_set=training_set,
-    registered_model_name=model_name
-  )
+fs.log_model(
+  wrappedModel,
+  "model",
+  flavor=mlflow.pyfunc,
+  training_set=training_set,
+  registered_model_name=model_name
+)
 
 # COMMAND ----------
 
