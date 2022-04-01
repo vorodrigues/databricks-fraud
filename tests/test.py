04897ff5ff43510c669b9f0e9d9e8c6c5e6770cb
vct_or@hotmail.com <vct_or@hotmail.com>
2022-04-01 17:06:14 +0000

    test-full

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+print('Starting test...')
+
+# COMMAND ----------
+
 from databricks_cli.sdk.api_client import ApiClient
 db = ApiClient(host='https://demo.cloud.databricks.com', token='dapi2331c6e00419d5466bad7d8bc58e2678', api_version='2.1')
 
@@ -13,18 +17,61 @@ import json
 
 f = {
     "settings": {
-        "existing_cluster_id": "0331-230110-ojy94y49",
-        "notebook_task": {
-            "notebook_path": "/Repos/victor.rodrigues@databricks.com/fraud-dev/Fraud 01: Data Engineering",
-            "base_parameters": {
-                "db": "vr_fraud_test",
-                "path": "/FileStore/vr/fraud/test"
-            }
-        },
         "timeout_seconds": 0,
         "email_notifications": {},
-        "name": "test-nb",
-        "max_concurrent_runs": 1
+        "name": "VR Fraud",
+        "max_concurrent_runs": 1,
+        "tasks": [
+            {
+                "existing_cluster_id": "0331-230110-ojy94y49",
+                "notebook_task": {
+                    "notebook_path": "/Repos/victor.rodrigues@databricks.com/fraud-dev/Fraud 01: Data Engineering",
+                    "base_parameters": {
+                        "db": "vr_fraud_test",
+                        "path": "/FileStore/vr/fraud/test"
+                    }
+                },
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "task_key": "data-engineering",
+                "description": ""
+            },
+            {
+                "existing_cluster_id": "0331-230110-ojy94y49",
+                "notebook_task": {
+                    "notebook_path": "/Repos/victor.rodrigues@databricks.com/fraud-dev/Fraud 02: Data Preparation",
+                    "base_parameters": {
+                        "db": "vr_fraud_test"
+                    }
+                },
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "task_key": "data-preparation",
+                "depends_on": [
+                    {
+                        "task_key": "data-engineering"
+                    }
+                ]
+            },
+            {
+                "existing_cluster_id": "0331-230110-ojy94y49",
+                "notebook_task": {
+                    "notebook_path": "/Repos/victor.rodrigues@databricks.com/fraud-dev/Fraud 04: Model Scoring",
+                    "base_parameters": {
+                        "db": "vr_fraud_test",
+                        "path": "/FileStore/vr/fraud/test"
+                    }
+                },
+                "timeout_seconds": 0,
+                "email_notifications": {},
+                "task_key": "model-scoring",
+                "depends_on": [
+                    {
+                        "task_key": "data-preparation"
+                    }
+                ]
+            }
+        ]
     }
 }
 
@@ -41,28 +88,29 @@ now = now.strftime('%Y-%m-%d-%H-%M-%S')
 print('Submitting run...')
 r = jobs.submit_run(
   run_name='vr-test-fraud-'+now, 
-  existing_cluster_id=settings['existing_cluster_id'],
-  notebook_task=settings['notebook_task']
+  tasks=settings['tasks']
 )
 run_id = r['run_id']
-print(run_id)
 
 # COMMAND ----------
 
 from time import sleep
+
+r = jobs.get_run(run_id=run_id)
+print(r['run_page_url'])
+
+wait = True if 'result_state' not in r['state'] else False
+
 wait = True
 while wait:
-  r = jobs.get_run_output(run_id=run_id)
-  state = r['metadata']['state']['life_cycle_state']
-  wait = True if 'result_state' not in r['metadata']['state'] else False
+  r = jobs.get_run(run_id=run_id)
+  wait = True if 'result_state' not in r['state'] else False
   sleep(5)
 
 # COMMAND ----------
 
-result = r['metadata']['state']['result_state']
-print(result)
-
-# COMMAND ----------
-
-if result != 'SUCCESS':
-  raise Exception('TEST FAILED!!!')
+result = r['state']['result_state']
+if result == 'SUCCESS':
+  print(result)
+else:
+  raise Exception(result+': '+r['state']['state_message'])
